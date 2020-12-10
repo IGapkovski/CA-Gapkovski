faa98566b71387d803d153a890b7bca345fbbfd3
Ilija Gapkovski <ilija_g@hotmail.com>
2020-12-10 01:59:06 +0100

    Main

--- CalcCas2/Program.cs
+++ CalcCas2/Program.cs
@@ -7,6 +7,17 @@ namespace CalcCas2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var a = 2;
+            var b = 5;
+            var c = 2;
+
+            var calc = new Calc("Casio");
+
+            Console.WriteLine("Zbirot od a i b e " + calc.Add(a, b));
+            Console.WriteLine(calc.Compare(a, b));
+            Console.WriteLine(calc.Compare(a, c));
+            Console.WriteLine(calc.Compare(b, c));
+            _ = Console.ReadLine();
         }
     }
 }
