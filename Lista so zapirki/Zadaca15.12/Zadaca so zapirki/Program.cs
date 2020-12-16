3552601cad664bab3747cce49427b821b29a58c9
Ilija Gapkovski <ilija_g@hotmail.com>
2020-12-16 23:20:39 +0100

    Finalna verzija

--- Lista so zapirki/Zadaca15.12/Zadaca so zapirki/Program.cs
+++ Lista so zapirki/Zadaca15.12/Zadaca so zapirki/Program.cs
@@ -5,20 +5,20 @@ namespace Zadaca_so_zapirki
 {
     class Program
     {
-        
+        static bool isSorted = false;
         static void Main(string[] args)
         {
             Console.WriteLine("Vnesi lista na broevi odvoeni so zapirki");
             string listaVneseniBroevi = Console.ReadLine();
             List<int> listaBroevi = KreirajLista(listaVneseniBroevi);
             PrintajLista(listaBroevi);
-            //var sortiraniBroevi = SortirajLista(listaBroevi);
-            //PrintajLista(sortiraniBroevi);
+            var sortiraniBroevi = SortirajLista(listaBroevi);
+            PrintajLista(sortiraniBroevi);
         }
 
         static List<int> KreirajLista(string lista)
         {
-            var sortiranaLista = new List<int>();
+            var nesortiranaLista = new List<int>();
             for (int i = 0; i < lista.Length; i++)
             {
                 string novBroj = "";
@@ -28,13 +28,39 @@ namespace Zadaca_so_zapirki
                     if (i == lista.Length - 1) break;
                     i++;
                 }
-                sortiranaLista.Add(Convert.ToInt32(novBroj));
+                nesortiranaLista.Add(Convert.ToInt32(novBroj));
+            }
+            return nesortiranaLista;
+        }
+
+        static List<int> SortirajLista(List<int> lista)
+        {
+            var sortiranaLista = new List<int>(lista.Count);
+            for (int i = 0; i < lista.Count; i++)
+            {
+                var tekovenBroj = lista[i];
+                var tekovenIndex = i;
+
+                while (tekovenIndex > 0 && sortiranaLista[tekovenIndex - 1] > tekovenBroj)
+                {
+                    tekovenIndex--;
+                }
+
+                sortiranaLista.Insert(tekovenIndex, tekovenBroj);
             }
             return sortiranaLista;
         }
+
         static void PrintajLista(List<int> lista)
         {
-            foreach(int broj in lista)
+            if (!isSorted)
+            {
+                Console.WriteLine("Vnesena Lista");
+                isSorted = true;
+            }
+            else Console.WriteLine("Sortirana lista");
+
+            foreach (int broj in lista)
             {
                 Console.WriteLine(broj);
             }
