a16ced33dc1d46a4aa9ee4c8285c4af533015b52
Ilija Gapkovski <75587314+IGapkovski@users.noreply.github.com>
2020-12-16 15:34:11 +0100

    Prv obid

--- Lista so zapirki/Zadaca15.12/Zadaca so zapirki/Program.cs
+++ Lista so zapirki/Zadaca15.12/Zadaca so zapirki/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Zadaca_so_zapirki
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Vnesi lista na broevi odvoeni so zapirki");
+            string listaVneseniBroevi = Console.ReadLine();
+            List<int> listaBroevi = KreirajLista(listaVneseniBroevi);
+            PrintajLista(listaBroevi);
+            //var sortiraniBroevi = SortirajLista(listaBroevi);
+            //PrintajLista(sortiraniBroevi);
+        }
+
+        static List<int> KreirajLista(string lista)
+        {
+            var sortiranaLista = new List<int>();
+            for (int i = 0; i < lista.Length; i++)
+            {
+                string novBroj = "";
+                while (lista[i] != ',')
+                {
+                    novBroj += lista[i];
+                    i++;
+                }
+                sortiranaLista.Add(Convert.ToInt32(novBroj));
+            }
+            return sortiranaLista;
+        }
+        static void PrintajLista(List<int> lista)
+        {
+            foreach(int broj in lista)
+            {
+                Console.WriteLine(broj);
+            }
         }
     }
 }
