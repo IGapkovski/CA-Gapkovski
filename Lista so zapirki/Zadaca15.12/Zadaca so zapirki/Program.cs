e4a0404c0f084991baf6225da22dcfea2e5bd811
Ilija Gapkovski <75587314+IGapkovski@users.noreply.github.com>
2020-12-16 14:28:14 +0100

    Nov proekt

---
+++ Lista so zapirki/Zadaca15.12/Zadaca so zapirki/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zadaca_so_zapirki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
