9f411256ff198d46deac872c20161a175e21b13c
Ilija Gapkovski <ilija_g@hotmail.com>
2021-01-08 18:17:30 +0100

    Final

---
+++ ZadaciZimski/Tocka/Tocka3D.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tocka
+{
+    class Tocka3D
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+    }
+}
