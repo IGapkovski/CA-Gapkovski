c0a518968c4e13284a5e64022a72c27f7f3343a8
Ilija Gapkovski <ilija_g@hotmail.com>
2021-01-11 00:09:27 +0100

    Final

---
+++ ZadaciZimski/UreduvanjeDom/Kukja.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UreduvanjeDom
+{
+    class Kukja
+    {
+        public Soba SobaKukja { get; set; }
+        public string Adresa { get; set; }
+    public Kukja()
+        {
+            SobaKukja = new Soba(); Adresa = "Partizanska 20"; 
+        }
+        public Kukja(Soba soba, string adresa)
+        {
+            SobaKukja = soba; Adresa = adresa;
+        }
+        public void Pecati()
+        {
+            Console.Write($"Adresa: {Adresa}, Soba: ");
+            SobaKukja.Pecati();
+        }
+    }
+}
---
+++ ZadaciZimski/UreduvanjeDom/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UreduvanjeDom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ureduvanje na dom");
+            Console.Write("Vnesi broj na objekti: ");
+            int n = int.Parse(Console.ReadLine());
+            var domovi = new List<Kukja>();
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"-----Dom {i+1}-----");
+                Console.Write("Sirina na masa: ");
+                int sirinaMasa = int.Parse(Console.ReadLine());
+                Console.Write("Dolzina na masa: ");
+                int dolzinaMasa = int.Parse(Console.ReadLine());
+                Console.Write("Sirina na soba: ");
+                int sirinaSoba = int.Parse(Console.ReadLine());
+                Console.Write("Dolzina na soba: ");
+                int dolzinaSoba = int.Parse(Console.ReadLine());
+                Console.Write("Adresa:");
+                string adresa = Console.ReadLine();
+                var dom = new Kukja(new Soba(new Masa(dolzinaMasa, sirinaMasa), dolzinaSoba, sirinaSoba), adresa);
+                domovi.Add(dom);
+            }
+            Console.WriteLine();
+            foreach(var dom in domovi)
+            {
+                dom.Pecati();
+            }
+        }
+    }
+}
