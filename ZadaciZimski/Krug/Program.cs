b46bc0cc40f9a56bd46796f614f1c66fe320d8ad
Ilija Gapkovski <ilija_g@hotmail.com>
2021-01-10 03:12:08 +0100

    Final

---
+++ ZadaciZimski/Krug/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Krug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Krug");
+            var krug = new Krug(5);
+            Console.WriteLine($"Perimetar na krugot = {krug.PresmetajPerimetar()}");
+            Console.WriteLine($"Plostina na krugot = {krug.PresmetajPlostina()}");
+            Console.WriteLine($"Dali Perimetarot i Plostinata se ednakvi? {krug.IsSame()}");
+        }
+    }
+}
