9b51de7d605db61e24e696ca45ddb11c0dfb714d
Ilija Gapkovski <ilija_g@hotmail.com>
2021-01-14 17:57:33 +0100

    final

---
+++ ZadaciZimski/SportskiEkipi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SportskiEkipi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sportski Ekipi");
+            Console.Write("Vnesi Ime: ");
+            string ime = Console.ReadLine();
+            Console.Write("Vnesi pobedi: ");
+            int pobedi = int.Parse(Console.ReadLine());
+            Console.Write("Vnesi porazi: ");
+            int porazi = int.Parse(Console.ReadLine());
+            Console.Write("Vnesi crveni kartoni: ");
+            int crveni = int.Parse(Console.ReadLine());
+            Console.Write("Vnesi zolti kartoni: ");
+            int zolti = int.Parse(Console.ReadLine());
+            Console.Write("Vnesi nereseni: ");
+            int nereseni = int.Parse(Console.ReadLine());
+            var fudbaleri = new FudbalskaEkipa(ime, porazi, pobedi, crveni, zolti, nereseni);
+            fudbaleri.Pecati();
+        }
+    }
+}
