e59bb060e1f255b23904d3d00b6ba989e0737951
Ilija Gapkovski <ilija_g@hotmail.com>
2021-01-20 20:45:26 +0100

    Final

---
+++ ZadaciZimski/SoncevSistem/Planeta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoncevSistem
+{
+    class Planeta
+    {
+        public string Ime { get; set; }
+        public int Dijametar { get; set; }
+        public int Oddalecenost { get; set; }
+        public float Masa { get; set; }
+        public Planeta()
+        {
+        }
+        public Planeta(string ime, int dijametar, int o, float masa)
+        {
+            Ime = ime; Dijametar = dijametar; Oddalecenost = o; Masa = masa;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"{Ime} so dijametar {Dijametar}km.\n");
+        }
+    }
+}
